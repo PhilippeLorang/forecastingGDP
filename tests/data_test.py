4b4fee0c1c57d239d9086a15b9c3301c1199c3fe
Laurent Lecigne <llecigne@free.fr>
2021-06-30 12:20:58 +0200

    Fix tests to limit Fred API usage
    Mainly to avoid HTTP 429 Too many request.

--- tests/data_test.py
+++ tests/data_test.py
@@ -4,14 +4,11 @@ from numpy.testing import assert_array_equal
 from forecastingGDP.data import CACHE_LOCATION, SERIES_ID, clear_cache, get_data
 
 def test_get_data():
-    data = get_data()
-    assert len(data) > 0
-    assert data.shape[1] == len(SERIES_ID)
-
-def test_get_data_cached():
-    data = get_data()
+    data = get_data(use_cache=True)
     data_cached = get_data(use_cache=True)
     assert isfile(CACHE_LOCATION)
+    assert len(data) > 0
+    assert data.shape[1] == len(SERIES_ID)
     assert data.shape == data_cached.shape
     assert data.index.dtype == data_cached.index.dtype
     assert_array_equal(data.dtypes, data_cached.dtypes)
